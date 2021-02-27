dc87a74dd5ebc5d26ac423b9105bd7186e07a033
leonardosibela <leonardo_sibela@hotmail.com>
2021-02-27 16:28:57 -0300

    Adding card widget into new file

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:list_app_flutter/quote.dart';
 import 'quote.dart';
+import 'quote_card.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -33,42 +34,6 @@ class _QuoteListState extends State<QuoteList> {
       ),
       body: Column(
         children: quotes.map((quote) => QuoteCard(quote: quote)).toList()
-      ),
-    );
-  }
-}
-
-class QuoteCard extends StatelessWidget {
-
-  final Quote quote;
-  QuoteCard({this.quote});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-      child: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              quote.text,
-              style: TextStyle(
-                fontSize: 18.0,
-                color: Colors.grey[600]
-              ),
-            ),
-            SizedBox(height: 6.0),
-            Text(
-              quote.author,
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800]
-              ),
-            )
-          ],
-        ),
       ),
     );
   }
